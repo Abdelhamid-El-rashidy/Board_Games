41136c4c8ac37faa4b5e95bbb8f424b880bc4ca4
Abdelhamid_ElRashidy <abdelhamidofficial2005@gmail.com>
2025-11-28 16:30:03 +0200

    Update Menu list and Cmake executable source

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,12 @@
 #include <memory>   // Required for unique_ptr
 
 #include "../include/BoardGame_Classes.h"
-#include "include/Four_Four_XO.h"
+#include "../include/Four_Four_XO.h"
 #include "../include/Infinity_XO_Classes.h"
-#include "include/Numerical_X_O.h"
+#include "../include/Numerical_X_O.h"
 #include "../include/Misere_Tic_Tac_Toe.h"
+#include "../include/SUS_Classes.h"
+#include "../include/X_O_5x5.h"
 using namespace std;
 
 /**
@@ -40,16 +42,21 @@ int main() {
     while(choice != 0) {
         cout << "--Welcome to the FCAI Board Game Collection:\n";
         cout << "------------------------------------------\n";
+        cout << "0. Exit\n";
         cout << "1. Infinity X_O\n";
         cout << "2. Numerical X_O\n";
         cout << "3. 5 x 5 X_O\n";
         cout << "4. Misere X_O\n";
         cout << "5. Four_Four X_O\n";
-        cout << "0. Exit\n";
+        cout << "6. SUS Game\n";
         cout << "Select a game (0-6): ";
         cin >> choice;
 
         switch (choice) {
+            case 0: {
+                cout << "Exiting the program. Goodbye!\\n";
+                return 0;
+            }
             case 1: {
                 UI<char>* game_ui = new Infinit_XO_UI();
 
@@ -218,20 +225,7 @@ int main() {
                 break;
         }
     }
-    
 
-    // // Create an instance of the specific UI for X-O using a pointer 
-    // UI<char>* game_ui = new Four_Four_XO_UI();
-
-    // // Create the game board. For X-O, this is an X_O_Board.
-    // Board<char>* xo_board = new Four_Four_XO_Board();
-
-    // // Use the UI to set up the players for the game.
-    // // The UI returns a dynamically allocated array of Player pointers.
-    // Player<char>** players = game_ui->setup_players();
-
-    // // Create the game manager with the board and the array of players.
-    // GameManager<char> x_o_game(xo_board, players, game_ui);
 
     // // Run the game loop.
     // x_o_game.run();
@@ -247,9 +241,6 @@ int main() {
     // // Delete the dynamically allocated array of player pointers itself.
     // delete[] players;
 
-    
-    cout << "Exiting the program. Goodbye!\\n";
-    
     return 0; // Exit successfully
 }
 
